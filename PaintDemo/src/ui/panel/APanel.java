8a24b8990e7f48cd45e2b2767078335000edb383
hojunh <hojunh@hojunh-PC>
2016-05-22 13:06:41 +0900

    Apanel 클릭했을때 콘솔에
    좌표 찍히게 만들어봤어요

--- PaintDemo/src/ui/panel/APanel.java
+++ PaintDemo/src/ui/panel/APanel.java
@@ -5,11 +5,26 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 
 import ui.PaintForm;
+import java.awt.Canvas;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class APanel extends JPanel {
-	
+	int x1,y1; //마우스 클릭 시작 지점
+	int x2, y2; 
 	public APanel() {
 		this.setSize(new Dimension(800, 600));
+		setLayout(null);
+		
+		Canvas canvas = new Canvas();
+		canvas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("click x : "+ e.getX());
+				System.out.println("click y : "+ e.getY());
+			}
+		});
+		canvas.setBounds(10, 10, 743, 580);
+		add(canvas);
 	}
-
 }
