7eb0e56a295ab35ead2567579fd2a93e09e42549
yun <yun@localhost>
2016-05-22 12:25:45 +0900

    CPanel commit

--- PaintDemo/src/ui/panel/CPanel.java
+++ PaintDemo/src/ui/panel/CPanel.java
@@ -5,10 +5,32 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 
 import ui.PaintForm;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class CPanel extends JPanel {
 	public CPanel(PaintForm paintForm) {
 		super();
 		this.setSize(new Dimension(800, 600));
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(65, 77, 61, 16);
+		add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblNewLabel.setText("1");
+			}
+		});
+		btnNewButton.setBounds(66, 105, 117, 29);
+		add(btnNewButton);
+		
+		
+		
+		
 	}
 }
