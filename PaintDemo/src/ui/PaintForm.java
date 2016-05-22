54fc61d78bf99e7add40b353cf2d4ddc21ff3f65
yun <yun@localhost>
2016-05-22 13:46:56 +0900

    Form 전체화면으로 변

--- PaintDemo/src/ui/PaintForm.java
+++ PaintDemo/src/ui/PaintForm.java
@@ -3,6 +3,8 @@ package ui;
 import java.awt.Dimension;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -11,53 +13,76 @@ import javax.swing.JFrame;
 import ui.panel.APanel;
 import ui.panel.BPanel;
 import ui.panel.CPanel;
+import javax.swing.JMenuBar;
+import java.awt.CardLayout;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
-public class PaintForm extends JFrame implements MouseListener {
+public class PaintForm extends JFrame implements ActionListener {
 	
+	private CardLayout cardLayout;
+	private JPanel panel;
+	
+	private APanel aPanel;
+	private BPanel bPanel;
 	private CPanel cPanel;
+	
+	private JMenuItem menuItemA;
+	private JMenuItem menuItemB;
+	private JMenuItem menuItemC;
 	
 	public PaintForm() {
 		super();
 		this.setSize(new Dimension(850, 650));
-		getContentPane().setLayout(new GridLayout(3, 0, 0, 0));
-		
-		APanel panel_1 = new APanel();
-		getContentPane().add(panel_1);
-		
-		BPanel panel_2 = new BPanel();
-		getContentPane().add(panel_2);
-		
-		CPanel panel = new CPanel();
-		getContentPane().add(panel);
 
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenu mnSelect = new JMenu("Select");
+		menuBar.add(mnSelect);
+		
+		menuItemA = new JMenuItem("A");
+		mnSelect.add(menuItemA);
+		
+		menuItemB = new JMenuItem("B");
+		mnSelect.add(menuItemB);
+		
+		menuItemC = new JMenuItem("C");
+		mnSelect.add(menuItemC);
+		
+		menuItemA.addActionListener(this);
+		menuItemB.addActionListener(this);
+		menuItemC.addActionListener(this);
+		
+		cardLayout = new CardLayout(0,0);
+		this.setLayout(cardLayout);
+		
+		aPanel = new APanel();
+		bPanel = new BPanel();
+		cPanel = new CPanel();
+		
+		getContentPane().add(aPanel, "A");
+		getContentPane().add(bPanel, "B");
+		getContentPane().add(cPanel, "C");
+		
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getActionCommand() == menuItemA.getText()) {
+			cardLayout.show(this.getContentPane(), "A");
+		}
+		else if(e.getActionCommand() == menuItemB.getText()) {
+			cardLayout.show(this.getContentPane(), "B");
+		}
+		else if(e.getActionCommand() == menuItemC.getText()) {
+			cardLayout.show(this.getContentPane(), "C");
+		}
 	}
 }
--- PaintDemo/src/ui/panel/CPanel.java
+++ PaintDemo/src/ui/panel/CPanel.java
@@ -12,24 +12,14 @@ import java.awt.event.ActionEvent;
 
 public class CPanel extends JPanel {
 	public CPanel() {
+		super();
 		this.setSize(new Dimension(800, 600));
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(65, 77, 61, 16);
-		add(lblNewLabel);
-		
 		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				lblNewLabel.setText("1");
-			}
-		});
-		btnNewButton.setBounds(66, 105, 117, 29);
+		btnNewButton.setBounds(53, 67, 117, 29);
 		add(btnNewButton);
 		
-		
-		
-		
+		this.setVisible(true);
 	}
 }
