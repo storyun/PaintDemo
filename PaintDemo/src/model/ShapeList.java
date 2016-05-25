122218ba94238402d8298f874751bd2a32e233c3
yun <yun@192.168.0.5>
2016-05-25 23:48:07 +0900

    shape list 수정

--- PaintDemo/src/model/ShapeList.java
+++ PaintDemo/src/model/ShapeList.java
@@ -1,7 +1,24 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ShapeList implements Serializable{
+	
+	private ArrayList<Shape> shapeList;
+	
+	public ShapeList() {
+		shapeList = new ArrayList<Shape>();
+		
+	}
 
+	public ArrayList<Shape> getShapeList() {
+		return shapeList;
+	}
+
+	public void setShapeList(ArrayList<Shape> shapeList) {
+		this.shapeList = shapeList;
+	}
+	
+	
 }
